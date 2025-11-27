7e23672eb5d8aa242bf105eed9114655c88ffe76
Leonardo Rocha <leonardoluisrochaa@gmail.com>
2025-11-26 22:16:24 -0300

    corrigirback

--- projeto/frontend/src/pages/Login.js
+++ projeto/frontend/src/pages/Login.js
@@ -118,7 +118,7 @@ function Login() {
             </div>
           </div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            CRM Shot Fair Brasil3
+            CRM Shot Fair Brasil
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Faça login em sua conta
