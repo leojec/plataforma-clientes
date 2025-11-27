3c300bf80822f46ed4cfcbe3b4399843e27e7b5e
Leonardo Rocha <leonardoluisrochaa@gmail.com>
2025-11-26 21:13:19 -0300

    testeworkflow

--- projeto/frontend/src/pages/Login.js
+++ projeto/frontend/src/pages/Login.js
@@ -118,7 +118,7 @@ function Login() {
             </div>
           </div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            CRM Shot Fair Brasil1
+            CRM Shot Fair Brasil2
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Faça login em sua conta
